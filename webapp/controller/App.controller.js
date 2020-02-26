50b81e90b266faa05372ce40957317624bbebaa7
fidschenberger <ch.pfisterer@icloud.com>
2020-02-26 19:44:24 +0100

    first steps

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.fidschenberger.wasteStatsApp.controller.App", {
+
+		onInit: function () {
+			this.wasteTypes = Object.freeze({
+				"PAPER": 1, 
+				"OTHER": 2, 
+				"GLAS": 3,
+				"ORGANIC": 4
+			});
+		}
+	});
+});

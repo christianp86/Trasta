24f85d21fb1015a63225ef9ae7dd16650483fa58
fidschenberger <ch.pfisterer@icloud.com>
2020-02-27 17:30:40 +0100

    added functionality to add waste entry to model

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -3,5 +3,23 @@ sap.ui.define([
 ], function (Controller) {
   "use strict";
 
-  return Controller.extend("com.fidschenberger.wasteStatsApp.controller.App", {});
+  return Controller.extend("com.fidschenberger.wasteStatsApp.controller.App", {
+
+    addWaste: function() {
+      var oModel = this.getView().getModel("waste_items");
+      var oNewItem = oModel.getProperty("/newWasteItem");
+      oNewItem.date = Date.now();
+      var aWaste = oModel.getProperty("/wasteItems").map(function (oWaste) { return Object.assign({}, oWaste); });
+    
+      aWaste.push(oNewItem);
+      oModel.setProperty("/wasteItems", aWaste);
+      for (var propt in oNewItem) {
+        oNewItem[propt] = "";
+      }
+
+      oModel.setProperty("/newWasteItem", oNewItem);
+
+    }
+
+  });
 });
